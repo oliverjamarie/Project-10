71b78351c50a4a29d2d45c7b781a266fd4e10fe9
swarlos2995 <oliverjamarie@gmail.com>
2018-05-07 23:28:27 -0400

    Update Twitface.java

--- proj10/socialNetwork/Twitface.java
+++ proj10/socialNetwork/Twitface.java
@@ -6,22 +6,27 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+
+
 public class Twitface implements Runnable{
 
-	private Graph <String> network;
-	private String url; // To Remove Later
+	private static Graph <String> network = new Graph <String> ();
+	private String url;
+	private static Object o = new Object();
+	private int threadNum;
 	
-	public Twitface(String url) {
-		network = new Graph <String>();
+	public Twitface(String url, int threadNum) {
 		this.url = new String (url);
+		this.threadNum = threadNum;
 	}
 	
 	public Twitface() {
-		network = new Graph <String>();
 		url = new String();
+		threadNum = 1000;
 	}
 
 	public boolean addUser(String name) {
+		
 		return network.addVertexToGraph(name);
 	}
 
@@ -30,7 +35,20 @@ public class Twitface implements Runnable{
 	}
 
 	public boolean friend(String user1, String user2) {
-		return network.addEdgeToGraph(user1, user2, 0);
+		
+		if (!network.isVertex(user1)) {
+			addUser(user1);
+		}
+		
+		if (!network.isVertex(user2)) {
+			addUser(user2);
+		}
+		
+		if (network.addUndirectedEdgeToGraph(user1, user2, 1)){
+			return true;
+		}
+			
+		return false;
 	}
 
 	public Collection<String> getFriends(String user) {
@@ -43,21 +61,27 @@ public class Twitface implements Runnable{
 
 	public Collection<String> peopleYouMayKnow(String user) {
 		Set<String> set = new HashSet <String> ();
+
 		
-		for (String msg : network.neighborsOfVertex(user)) {
-			if (!user.equals(msg)) {
-				set.add(msg);
-			}
+		for (String friend : network.neighborsOfVertex(user)) {
+				for (String suggestion: network.neighborsOfVertex(friend)) {
+					if (!network.hasEdge(user, suggestion) && !user.equals(suggestion)) {
+						set.add(suggestion);
+					}
+				}
 		}
 		
 		return set;
 	}
 
 	public void readFriendData(List<String> urls) {
+		
+		int count = 0;
 		List<Thread> threads = new ArrayList<Thread>();
 		
 		for (String url : urls) {
-			Thread thread = new Thread(new Twitface(url));
+			Thread thread = new Thread (new Twitface(url,count));
+			count++;
 			threads.add(thread);
 		}
 		
@@ -74,10 +98,37 @@ public class Twitface implements Runnable{
 		}
 		
 	}
+	
+	public String toString() {
+		return network.toString();
+	}
 
 	@Override
 	public void run() {
-		System.out.println(url);
+		try {
+			CommandReader commandRead = new CommandReader(url);
+			
+			for (Command cmd : commandRead.getCommands()) {
+				
+				if (cmd.getCommand().equalsIgnoreCase("addPerson")) {
+					synchronized (o) {
+						addUser(cmd.getUser());
+						
+					}
+				} else if (cmd.getCommand().equalsIgnoreCase("addFriend")) {
+					synchronized (o) {
+						String user = cmd.getUser();
+						String friend = cmd.getFriend();
+						
+						friend(user,friend);
+						
+					}
+				}
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
